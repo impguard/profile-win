360e5526a0f958cf7a31e55006523bea487ba869
Kevin Wu <contact@kevinwu.io>
2023-03-11 23:29:48 -0800

    Updated install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,10 @@
-echo hi
+$name  = Read-Host "Github Name : "
+$email = Read-Host "Github Email: "
+
+
+git config --global user.name "$name"
+git config --global user.email "$email"
+
+git clone git@github.com:impguard/profile-win.git "$HOME/.profile.d"
+
+Write-Output "Please 'cd $HOME/.profile.d' and run the setup manually."
