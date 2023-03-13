e890d437ce5a1008be4eda8e3642d9f8e44e85ca
Kevin Wu <contact@kevinwu.io>
2023-03-12 23:06:13 -0700

    Work in progress on setup script

---
+++ setup.ps1
@@ -0,0 +1,48 @@
+Param(
+  [Parameter(ParameterSetName = "Home")]
+  [switch]$HomeParam
+)
+
+$ROOT = "$HOME\.profile.d"
+$PROFILE_DIR = "$ROOT\profile"
+$STAGING_DIR = "$ROOT\staging"
+$INSTALL_DIR = "$ROOT\staging\install"
+
+########################################
+# Helpers
+########################################
+
+function Log {
+  Write-Host $args -ForegroundColor Green
+}
+
+function Stage {
+  $Source = $PROFILE_DIR.Replace("\", "\\")
+
+  $Items = Get-ChildItem -LiteralPath $Source -Recurse
+
+  Write-Output $Source
+  # Write-Output $Items
+}
+
+########################################
+# Commands
+########################################
+
+function Install {
+  Home
+}
+
+function Home {
+  Log "Linking home files"
+}
+
+if ($HomeParam) {
+  Stage
+  Home
+}
+else {
+  Stage
+  Install
+}
+
